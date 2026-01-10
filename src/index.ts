a8eb23522178e79b070a55a75fda94a81ac7524c
Keval Tank <kevaltank1445@gmail.com>
2026-01-10 13:51:31 +0530

    configure better auth and resend email

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,21 @@
 import express from "express"
+import {toNodeHandler} from "better-auth/node"
+import { auth } from "./lib/auth"
+import cors from 'cors'
 
 const app = express()
 const PORT = process.env.PORT
+
+app.all("/api/auth/*splat", toNodeHandler(auth))
+
+app.use(cors({
+    origin : process.env.FRONTEND_URL,
+    methods : ['GET','POST','PUT','PATCH','OPTIONS',"DELETE"],
+    credentials : true
+}))
+app.use(express.json())
+app.use(express.urlencoded({extended : true}))
+
+
+
 app.listen(PORT, () => console.log(`server running at ${PORT}`))
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,27 @@
 import { betterAuth } from "better-auth";
 import { prisma } from "./prisma";
 import {emailOTP} from 'better-auth/plugins'
+import { sendSignInEmail } from "../services/sendEmail";
 
 export const auth = betterAuth({
-    database: prisma
+    database: prisma,
+    socialProviders : {
+        google : {
+            clientId : process.env.GOOGLE_CLIENT_ID!,
+            clientSecret : process.env.GOOGLE_CLIENT_SECRET
+        }
+    },
+    plugins : [
+        emailOTP({
+            async sendVerificationOTP({email, otp} : {email:string, otp:string}) {
+                const response = await sendSignInEmail(email, otp)
+                if(!response.success){
+                    throw new Error(response.error || "Failed to send OTP")
+                }
+                console.log(`Sign in email sent : ${response.id}`)
+            },
+            otpLength : 6,
+            expiresIn : 300
+        })
+    ]
 });
