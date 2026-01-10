4469608b47158638d49f5b38c61f17c46a9da34d
Keval Tank <kevaltank1445@gmail.com>
2026-01-10 15:43:33 +0530

    Organize project structure

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,20 @@
 import express from "express"
-import {toNodeHandler} from "better-auth/node"
-import { auth } from "./lib/auth"
-import cors from 'cors'
+import { corsOptions } from "./config/cors"
+import apiRoutes from './routes'
+import { errorHandler, notFoundHandler } from "./middleware/errorHandler"
+
 
 const app = express()
 const PORT = process.env.PORT
 
-app.use(cors({
-    origin : process.env.FRONTEND_URL,
-    methods : ['GET','POST','PUT','PATCH','OPTIONS',"DELETE"],
-    credentials : true
-}))
+app.use(corsOptions)
 app.use(express.json())
 app.use(express.urlencoded({extended : true}))
 
 
-app.all("/api/auth/*", toNodeHandler(auth))
+app.use('/api/', apiRoutes)
 
-
+app.use(notFoundHandler)
+app.use(errorHandler)
 
 app.listen(PORT, () => console.log(`server running at ${PORT}`))
