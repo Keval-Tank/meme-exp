a6884a4de6087ec0223569574067761654bc4dcf
Keval Tank <innewdesktop3@Innews-Mac-mini-2.local>
2026-01-13 17:31:55 +0530

    intialized supabase client

---
+++ src/lib/supabaseClient.ts
@@ -0,0 +1,11 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabase_project_url = process.env.SUPABASE_PROJECT_URL
+const supabase_publishable_key= process.env.SUPABASE_PUBLISHABLE_KEY
+
+let supabaseClient
+if(!supabase_project_url || !supabase_publishable_key){
+    throw new Error("Supabase credentials are missing")
+}
+
+export const supabase = createClient(supabase_project_url, supabase_publishable_key)
