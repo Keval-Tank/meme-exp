44bfca3a1d6bd557babc53c15323892dd14a60b2
Keval Tank <innewdesktop3@Innews-Mac-mini-2.local>
2026-01-15 19:50:05 +0530

    removed unneccessary betterauth files and remove extra spaces from src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,21 @@
 import express from "express"
 import { corsOptions } from "./config/cors"
 import { errorHandler, notFoundHandler } from "./middleware/errorHandler"
-import { toNodeHandler } from "better-auth/node"
-import { auth } from "./lib/auth"
 import dotenv from 'dotenv'
 import { generalRateLimiter } from "./middleware/rateLimiter"
 import apiRoutes from './routes'
 
-
 dotenv.config()
-
 
 const app = express()
 const PORT = process.env.PORT
 
-
 app.use(corsOptions)
 app.use(express.json())
-
-// app.all("/api/auth/{*any}", toNodeHandler(auth))
+app.use(express.urlencoded({extended : true}))
 
 app.use(generalRateLimiter)
 app.use("/api", apiRoutes)
-
 
 app.use(notFoundHandler)
 app.use(errorHandler)
